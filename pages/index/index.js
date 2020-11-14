db15721907902aa93a761f5949feee736ddff316
李俊杰 <1061455426@qq.com>
2020-11-14 22:13:14 +0800

    navigate

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,5 +6,22 @@ Page({
   data: {
     
   },
-  
+
+  tap1: function(){
+    wx.navigateTo({
+      url: '/pages/subpage1/index',
+    })
+  },
+
+  tap2:function(){
+    wx.navigateTo({
+      url: '/pages/subpage2/index',
+    })
+  },
+
+  tap3:function(){
+    wx.navigateTo({
+      url: '/pages/subpage3/index',
+    })
+  }
 })
